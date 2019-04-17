49c72bd782d3030f90783cb1d028ebbf23065571
Eric J Fisher <eric-fisher@pluralsight.com>
2019-04-16 21:35:13 -0400

    Task 1 Create GetLetterGrade Override Complete

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,22 @@
-﻿namespace GradeBook.GradeBooks
+﻿using System;
+
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("You must have at least 5 students to do ranked grading.");
+            }
+
+            return 'F';
         }
     }
 }
