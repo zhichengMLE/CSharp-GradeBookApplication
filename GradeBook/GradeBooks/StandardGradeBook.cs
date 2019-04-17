15df0aaac2e1c79c6953f6a0d11f6246efd8027a
Eric J Fisher <eric-fisher@pluralsight.com>
2019-04-16 21:22:15 -0400

    Task 3 Create StandardGradeBook class

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,10 @@
+﻿namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Name = name;
+        }
+    }
+}
