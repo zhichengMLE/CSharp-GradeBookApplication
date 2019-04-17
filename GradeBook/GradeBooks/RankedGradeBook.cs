4373598c36ee459f88ae0911dc8dc8ea4c830ced
Eric J Fisher <eric-fisher@pluralsight.com>
2019-04-16 21:51:11 -0400

    Task 1 Create CalculateStatistics Override Complete

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -30,5 +30,16 @@ namespace GradeBook.GradeBooks
                 return 'D';
             return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count() < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
     }
 }
