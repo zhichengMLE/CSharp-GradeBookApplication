fa138a2668f0d9e67b5a476bd2321b9a7e57be28
zhichengMLE <jason-dean@outlook.com>
2020-01-15 17:00:39 -0800

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,41 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            List<double> grades = new List<double>();
+            foreach(Student student in Students)
+            {
+                grades.Add(student.AverageGrade);
+            }
+
+            grades.Sort();
+
+            if(averageGrade >= grades[threshold-1])
+            {
+                return 'A';
+            }
+            else if(averageGrade >= grades[threshold-2])
+            {
+                return 'B';
+            }
+            else if(averageGrade >= grades[threshold-3])
+            {
+                return 'C';
+            }
+            else
+            {
+                return 'D';
+            }
+
+            return 'R';
+        }
     }
 }
